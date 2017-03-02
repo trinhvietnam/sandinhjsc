6fee680e584021f9a0bd603cd184631dda273e53
Trinh Viet Nam <trinhvietnambk@gmail.com>
2017-03-02 17:18:42 +0700

    no message

--- wp-content/themes/onepress/footer.php
+++ wp-content/themes/onepress/footer.php
@@ -83,12 +83,11 @@ do_action( 'onepress_site_end' );
 <script type='text/javascript'
         src='https://maps.google.com/maps/api/js?key=AIzaSyC97ib0SwAgdFsndmPYKZr3Gl9ODCK6G-4&sensor=false&#038;ver=4.6.3'></script>
 <?php
-    if(is_home()){ ?>
+    if(true){ ?>
         <script>
             $('.main-navigation .menu-item a').click(function () {
                 window.history.pushState("", "", $(this).attr('href'));
             });
-
         </script>
     <?php }
 ?>
